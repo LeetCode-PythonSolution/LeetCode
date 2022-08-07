d00870fb7eb97c43457c399811e5056bd2cec5a6
MahadevG <mahadev.merahkee@gmail.com>
2022-08-08 00:12:01 +0530

    Another

--- reverse_number.py
+++ reverse_number.py
@@ -9,3 +9,13 @@ def reverse(n):
     return rev
 
 print(reverse(24567))
+
+
+def reverse_positiveAnd_NegativeNo(input, rinput=0):
+    if input<0:
+        input=input-(input*2)
+        print(-reverse(input))
+    else:
+        print(reverse(input))
+
+reverse_positiveAnd_NegativeNo(-123)
